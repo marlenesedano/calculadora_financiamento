275c8b93000cfac600a54693fd568db82a7516ea
Marlene Sedano <marlenesedanodasilva@gmail.com>
2021-03-30 21:32:20 -0400

    refactor: Refatorando código

--- app.js
+++ app.js
@@ -1,55 +1,54 @@
-let value;
-let fees;
-let time;
-let cash;
-
-function tipe_fees(fees) {
-  let select_fees = document.getElementById("select_fees").value;
-  if (select_fees == "ano") {
-    fees = document.getElementById("fees-financing").value;
-
-    let new_fees = fees / 12;
-    fees = new_fees;
-    return fees;
-  } else {
-    fees = document.getElementById("fees-financing").value;
-    return fees;
-  }
-}
-
-function tipe_time(time) {
-  let select_time = document.getElementById("select_time").value;
-  if (select_time == "anual") {
-    time = document.getElementById("time-financing").value;
-
-    let new_time = time * 12;
-    time = new_time;
-    return time;
-  } else {
-    time = document.getElementById("time-financing").value;
-    return time;
-  }
-}
-
-function portion(value, fees, time, cash) {
-  value = document
+function getFinancingData() {
+  const amount = document
     .getElementById("value")
     .value.replace(".", "")
     .replace(",", ".");
-  time = tipe_time();
 
-  fees = tipe_fees();
-  let value_final;
+  const interest = document.getElementById("fees-financing").value;
+  const time = document.getElementById("time-financing").value;
+  const selectInterest = document.getElementById("select_fees").value;
+  const selectTime = document.getElementById("select_time").value;
 
-  fees = fees / 100;
-  data1 = Math.pow(1 + fees, time) - 1;
-  data2 = Math.pow(1 + fees, time) * fees;
-  value_final = value / (data1 / data2);
-  cash = value_final.toLocaleString("pt-br", {
+  return {
+    amount,
+    interest,
+    time,
+    selectInterest,
+    selectTime
+  }
+}
+
+function typeFinancingInterest({ interest, selectInterest }) {
+  if (selectInterest == "ano") {
+    let new_fees = interest / 12;
+    return new_fees;
+  } else {
+    return interest;
+  }
+}
+
+function typeFinancingTime({ time, selectTime }) {
+  if (selectTime == "anual") {
+    let new_time = time * 12;
+    return new_time;
+  } else {
+    return time;
+  }
+}
+
+function portion({ amount, time, interest }) {
+  time = typeFinancingTime(getFinancingData());
+  interest = typeFinancingInterest(getFinancingData()) / 100;
+
+  let data1 = Math.pow(1 + interest, time) - 1;
+  let data2 = Math.pow(1 + interest, time) * interest;
+  let value_final = amount / (data1 / data2);
+  let cash = value_final.toLocaleString("pt-br", {
     style: "currency",
     currency: "BRL",
   });
-  cash = document.getElementById("portion").innerHTML = cash;
+
+  document.getElementById("portion").innerHTML = cash;
   return value_final;
 }
 
@@ -66,13 +65,13 @@ function loan() {
   print = document.getElementById("total_loan").innerHTML = print;
 }
 
-function fees_acumulado(value, time) {
+function accumulated_interest() {
   value = document
     .getElementById("value")
     .value.replace(".", "")
     .replace(",", ".");
-  time = tipe_time();
-  let result = portion() * time;
+  time = typeFinancingTime(getFinancingData());
+  let result = portion(getFinancingData()) * time;
   let result_final = result - value;
   let print;
   print = result_final.toLocaleString("pt-br", {
@@ -82,10 +81,10 @@ function fees_acumulado(value, time) {
   print = document.getElementById("accumulated").innerHTML = print;
 }
 
-function payment_total() {
+function payment_total(portion_value) {
   value = document.getElementById("value").value;
-  time = tipe_time();
-  let result = time * portion();
+  time = typeFinancingTime(getFinancingData());
+  let result = time * portion_value;
   let print;
   print = result.toLocaleString("pt-br", {
     style: "currency",
@@ -94,21 +93,17 @@ function payment_total() {
   print = document.getElementById("payment").innerHTML = print;
 }
 
-// BOTÃO CALCULAR
 document.getElementById("calculate").addEventListener("click", () => {
-  tipe_time(time);
-  tipe_fees(fees);
-  portion(value, fees, time, cash);
+  let portion_value = portion(getFinancingData());
   loan();
-  fees_acumulado(value, time, fees);
-  payment_total();
+  accumulated_interest();
+  payment_total(portion_value);
 });
 
-// FUNÇÃO PARA LIMPAR CAMPOS
 function data_clear() {
   value = document.getElementById("value").value;
-  time = tipe_time();
-  fees = tipe_fees();
+  time = typeFinancingTime(getFinancingData());
+  fees = typeFinancingInterest(getFinancingData());
   value = "";
   time = "";
   fees = "";
@@ -121,7 +116,6 @@ function data_clear() {
   cash = document.getElementById("portion").innerHTML = "R$ 0,00";
 }
 
-// BOTÃO LIMPAR
 document.getElementById("clear").addEventListener("click", () => {
   data_clear();
 });
